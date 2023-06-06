c623d641f81147343b7be8d59caad38786f3deea
j <penguinjinbei@gmail.com>
2023-06-07 03:05:07 +0900

    feat: helper切り出し

---
+++ src/helper/aws/ses_client.rb
@@ -0,0 +1,22 @@
+require 'aws-sdk-ses'
+
+module Helper
+  module AWS
+    class SesClient
+      def initialize
+        @client = Aws::SES::Client.new(region: 'ap-northeast-1', endpoint: 'http://localhost:4566')
+      end
+
+      def send_raw_email(source, destinations, raw_message)
+        @client.verify_email_identity(email_address: source)
+        @client.send_raw_email({
+                                 destinations: destinations,
+                                 raw_message: {
+                                   data: raw_message
+                                 },
+                                 source: source
+                               })
+      end
+    end
+  end
+end
--- src/service/send_email_with_file_service.rb
+++ src/service/send_email_with_file_service.rb
@@ -1,4 +1,4 @@
-require 'aws-sdk-ses'
+require './src/helper/aws/ses_client'
 
 module SendEmailWithFile
   class Service
@@ -7,15 +7,7 @@ module SendEmailWithFile
       @destinations = ['to@example.com']
       @raw_message = 'Subject: Test email (contains an attachment)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\n#{text}\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename="attachment.txt"\n\n--NextPart--'
 
-      @client = Aws::SES::Client.new(region: 'ap-northeast-1', endpoint: 'http://localhost:4566')
-      @client.verify_email_identity(email_address: @source)
-      @client.send_raw_email({
-                               destinations: @destinations,
-                               raw_message: {
-                                 data: @raw_message
-                               },
-                               source: @source
-                             })
+      Helper::AWS::SesClient.new.send_raw_email(@source, @destinations, @raw_message)
 
       'OK'
     end
