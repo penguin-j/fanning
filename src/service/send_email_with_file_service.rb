c01da35f584417eca27798ee95a8a3941f767454
j <penguinjinbei@gmail.com>
2023-06-07 02:58:42 +0900

    feat: 変数切り出し

--- src/service/send_email_with_file_service.rb
+++ src/service/send_email_with_file_service.rb
@@ -1,20 +1,22 @@
-require 'json'
 require 'aws-sdk-ses'
 
 module SendEmailWithFile
   class Service
     def execute
+      @source = 'from@example.com'
+      @destinations = ['to@example.com']
+      @raw_message = 'Subject: Test email (contains an attachment)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\n#{text}\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename="attachment.txt"\n\n--NextPart--'
+
       @client = Aws::SES::Client.new(region: 'ap-northeast-1', endpoint: 'http://localhost:4566')
-      @client.verify_email_identity(email_address: 'from@example.com')
+      @client.verify_email_identity(email_address: @source)
       @client.send_raw_email({
-                               destinations: [
-                                 'to@example.com'
-                               ],
+                               destinations: @destinations,
                                raw_message: {
-                                 data: 'Subject: Test email (contains an attachment)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\nThis is the message body.\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename="attachment.txt"\n\nThis is the text in the attachment.\n\n--NextPart--'
+                                 data: @raw_message
                                },
-                               source: 'from@example.com'
+                               source: @source
                              })
+
       'OK'
     end
   end
