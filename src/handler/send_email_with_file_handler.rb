80700c577c54aed6378bf7cd0cf29a0c298fec2b
j <penguinjinbei@gmail.com>
2023-06-07 03:12:10 +0900

    feat: serviceクラス#executeに引数追加

--- src/handler/send_email_with_file_handler.rb
+++ src/handler/send_email_with_file_handler.rb
@@ -3,7 +3,14 @@ require './src/service/send_email_with_file_service'
 module SendEmailWithFile
   class Handler
     def self.execute(event:, context:)
-      @result = SendEmailWithFile::Service.new.execute
+      # TODO
+      @req = {
+        'source' => 'from@example.com',
+        'destinations' => ['to@example.com'],
+        'raw_message' => 'Subject: Test email (contains an attachment)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\n#{text}\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename="attachment.txt"\n\n--NextPart--'
+      }
+
+      @result = SendEmailWithFile::Service.new.execute(@req)
       {
         statusCode: 200,
         body: {
--- src/service/send_email_with_file_service.rb
+++ src/service/send_email_with_file_service.rb
@@ -2,10 +2,10 @@ require './src/helper/aws/ses_client'
 
 module SendEmailWithFile
   class Service
-    def execute
-      @source = 'from@example.com'
-      @destinations = ['to@example.com']
-      @raw_message = 'Subject: Test email (contains an attachment)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\n#{text}\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename="attachment.txt"\n\n--NextPart--'
+    def execute(req)
+      @source = req['source']
+      @destinations = req['destinations']
+      @raw_message = req['raw_message']
 
       Helper::AWS::SesClient.new.send_raw_email(@source, @destinations, @raw_message)
 
