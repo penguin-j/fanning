9bf98e36fc9110d5e892bcec4612845ec3659a8a
j <penguinjinbei@gmail.com>
2023-06-08 02:16:27 +0900

    feat: raw_messageオブジェクトをmailライブラリで生成する

--- src/service/send_email_with_file_service.rb
+++ src/service/send_email_with_file_service.rb
@@ -1,15 +1,29 @@
 require './src/helper/aws/ses_client'
+require 'mail'
 
 module SendEmailWithFile
   class Service
     def execute(req)
       @source = req['source']
       @destinations = req['destinations']
-      @raw_message = req['raw_message']
+      @raw_message = construct_raw_message
 
       Helper::AWS::SesClient.new.send_raw_email(@source, @destinations, @raw_message)
 
       'OK'
+    end
+
+    private
+
+    def construct_raw_message
+      @mail = Mail.new
+      @mail.subject = 'Here is a subject.'
+      @mail.body = 'Here is a body.'
+      @mail['content-type'] = 'text/plain'
+      @mail['MIME-Versio'] = '1.0'
+      @mail.add_file('./temp/attachment.txt')
+
+      @mail.to_s
     end
   end
 end
